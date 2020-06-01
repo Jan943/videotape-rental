2e609416f042caa6cadbe50059bb4d75c0dcde03
Jan943 <jansopata10@gmail.com>
2020-06-01 22:14:59 +0200

    Creating model and GetMapping in API:
    -creating list of cassettes in constructor of VideoCassetteApi
    -GetMapping in two ways: by id and all cassettes

---
+++ src/main/java/pl/sopata/videotaperental/VideotapeRentalApplication.java
@@ -0,0 +1,13 @@
+package pl.sopata.videotaperental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideotapeRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideotapeRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sopata/videotaperental/VideotapeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sopata.videotaperental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideotapeRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
