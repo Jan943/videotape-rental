7a114b2859319c7287399014810548e171306b23
Jan943 <jansopata10@gmail.com>
2020-06-02 22:52:04 +0200

    Finish simple CRUD API:
    -creating delete, post and put methods

--- src/main/java/pl/sopata/videotaperental/VideoCassetteApi.java
+++ src/main/java/pl/sopata/videotaperental/VideoCassetteApi.java
@@ -1,9 +1,6 @@
 package pl.sopata.videotaperental;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -11,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/cassettes/")
+@RequestMapping("/api/cassettes")
 public class VideoCassetteApi {
 
     private List<VideoCassette> videoCassettes;
@@ -19,7 +16,7 @@ public class VideoCassetteApi {
     public VideoCassetteApi() {
         this.videoCassettes = new ArrayList<>();
         videoCassettes.add(new VideoCassette(1L, "Titanic", LocalDate.of(1995,1,1)));
-        videoCassettes.add(new VideoCassette(1L, "Pulp Fiction", LocalDate.of(1990,2,2)));
+        videoCassettes.add(new VideoCassette(2L, "Pulp Fiction", LocalDate.of(1990,2,2)));
     }
 
     @GetMapping("/all")
@@ -32,5 +29,20 @@ public class VideoCassetteApi {
          Optional<VideoCassette> first = videoCassettes.stream().
                  filter(element -> element.getId() == index).findFirst();
          return first.get();
+    }
+
+    @PostMapping
+    public boolean addVideo(@RequestBody VideoCassette videoCassette) {
+        return videoCassettes.add(videoCassette);
+    }
+
+    @PutMapping
+    public boolean updateVideo(@RequestBody VideoCassette videoCassette) {
+        return videoCassettes.add(videoCassette);
+    }
+
+    @DeleteMapping
+    public boolean deleteVideo(@RequestParam int index) {
+        return videoCassettes.removeIf(element -> element.getId() == index);
     }
 }
